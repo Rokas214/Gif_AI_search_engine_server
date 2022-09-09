caf7b7e60d06d7d4a80adc06b03956be130b24d1
Rokas214 <rokas.guzys@gmail.com>
2022-09-09 14:19:32 +0300

    initialize gif search engine server side

---
+++ src/routes/v1/gifHistory.js
@@ -0,0 +1,41 @@
+const { dbConfig } = require("../../config");
+const express = require("express");
+const mysql = require("mysql2/promise");
+const router = express.Router();
+const Joi = require("joi");
+const { isLoggedIn } = require("../../midleware");
+
+const userSchema = Joi.object({
+	img: Joi.string().min(6).max(255).required(),
+	title: Joi.string().min(6).max(255).required(),
+	author: Joi.string().min(6).max(255).required(),
+	year: Joi.number().integer().max(2022),
+	description: Joi.string().min(6).max(149).required(),
+	price: Joi.number().integer().min(0).max(2022),
+});
+
+router.post("/", isLoggedIn, async (req, res) => {
+	let input = req.body;
+
+	try {
+		input = await userSchema.validateAsync(input);
+	} catch (err) {
+		return res.send({
+			err: "Something went wrong, make sure you fill the input",
+		});
+	}
+
+	try {
+		const con = await mysql.createConnection(dbConfig);
+		const [data] = await con.execute(`
+        INSERT INTO gifs (url)
+        VALUES ('${input.url}')
+        `);
+		await con.end();
+		return res.send(data);
+	} catch (err) {
+		return res.status(400).send(err);
+	}
+});
+
+module.exports = router;
