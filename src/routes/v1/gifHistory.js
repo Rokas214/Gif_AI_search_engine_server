e3976981ad21c4dd167fff449f3ddc0e471d3355
Rokas214 <rokas.guzys@gmail.com>
2022-09-10 23:12:06 +0300

    gif history validation

--- src/routes/v1/gifHistory.js
+++ src/routes/v1/gifHistory.js
@@ -5,13 +5,27 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
 	let input = req.body;
-
 	try {
 		const con = await mysql.createConnection(dbConfig);
 		const [data] = await con.execute(`
-        INSERT INTO gif_history (url,email)
-        VALUES ('${input.url}', '${req.headers.email}')
-        `);
+		SELECT url
+		FROM gif_history
+		ORDER BY id DESC
+		LIMIT 1;
+		`);
+		await con.end();
+		if (data[0].url === req.body.url) {
+			return res.send("same");
+		}
+	} catch (err) {
+		return res.status(400).send(err);
+	}
+	try {
+		const con = await mysql.createConnection(dbConfig);
+		const [data] = await con.execute(`
+	        INSERT INTO gif_history (url,email)
+	        VALUES ('${input.url}', '${req.headers.email}')
+	        `);
 		await con.end();
 		return res.send(data);
 	} catch (err) {
--- src/routes/v1/home.js
+++ src/routes/v1/home.js
@@ -1,9 +1,22 @@
+const { dbConfig } = require("../../config");
 const express = require("express");
-
+const mysql = require("mysql2/promise");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	res.send("hello");
+	try {
+		const con = await mysql.createConnection(dbConfig);
+		const [data] = await con.execute(`
+        SELECT DISTINCT url
+		FROM gif_history
+		ORDER BY id DESC
+		LIMIT 5;
+        `);
+		await con.end();
+		return res.send(data);
+	} catch (err) {
+		return res.status(400).send(err);
+	}
 });
 
 module.exports = router;
