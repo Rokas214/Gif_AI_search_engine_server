d99d05476a33ec4984893ff8bc33ae9068243d4e
Rokas214 <rokas.guzys@gmail.com>
2022-09-12 23:27:12 +0300

    Last 5 searches for user

--- src/routes/v1/home.js
+++ src/routes/v1/home.js
@@ -1,14 +1,17 @@
-const { dbConfig } = require("../../config");
 const express = require("express");
+const { dbConfig } = require("../../config");
 const mysql = require("mysql2/promise");
+const { isLoggedIn } = require("../../midleware");
+
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", isLoggedIn, async (req, res) => {
 	try {
 		const con = await mysql.createConnection(dbConfig);
 		const [data] = await con.execute(`
         SELECT DISTINCT url
 		FROM gif_history
+		WHERE email=('${req.headers.email}')
 		ORDER BY id DESC
 		LIMIT 5;
         `);
