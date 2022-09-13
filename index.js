f9f9c7eb268ab68ea3a23553ac6bfa21cc5b4995
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 10:44:46 +0300

    Testing aws

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const home = require("./src/routes/v1/home");
 const gifHistory = require("./src/routes/v1/gifHistory");
 const watsonUrlReader = require("./src/routes/v1/watsonUrlReader");
 const watsonTextReader = require("./src/routes/v1/watsonTextReader");
+const aws = require("./src/routes/v1/aws");
 
 app.use("/auth", auth);
 app.use("/", home);
 app.use("/gifhistory", gifHistory);
 app.use("/watson-url", watsonUrlReader);
 app.use("/watson-text", watsonTextReader);
+app.use("/aws", aws);
 
 app.listen(port, () => console.log("it's working"));
