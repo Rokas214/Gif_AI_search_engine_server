2e231e7262e8ea10203f905031a4176ad43e513f
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 11:29:31 +0300

    testing aws

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 module.exports = {
 	port: process.env.PORT || 3000,
-	jwt_secret: rokas123,
+	jwt_secret: "rokas123",
 	dbConfig: {
-		host: sql11.freemysqlhosting.net,
-		user: sql11518356,
-		password: YSxfC1asJA,
+		host: "sql11.freemysqlhosting.net",
+		user: "sql11518356",
+		password: "YSxfC1asJA",
 		port: 3306,
-		database: sql11518356,
+		database: "sql11518356",
 	},
 };
