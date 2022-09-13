b122645ffe035982a6835f395a6efaa124a3522b
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 16:51:24 +0300

    Azure added

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { port } = require("./src/config");
 
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 8080;
 const auth = require("./src/routes/v1/auth");
 const home = require("./src/routes/v1/home");
 const gifHistory = require("./src/routes/v1/gifHistory");
 const watsonUrlReader = require("./src/routes/v1/watsonUrlReader");
 const watsonTextReader = require("./src/routes/v1/watsonTextReader");
-const aws = require("./src/routes/v1/aws");
 
 app.use("/auth", auth);
 app.use("/", home);
 app.use("/gifhistory", gifHistory);
 app.use("/watson-url", watsonUrlReader);
 app.use("/watson-text", watsonTextReader);
-app.use("/aws", aws);
 
-app.listen(port, () => console.log("it's working"));
+app.listen(port, () => console.log(`Server working on port ${port}`));
