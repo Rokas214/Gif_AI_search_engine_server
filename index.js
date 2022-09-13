ef120adfc1a989a05ce4c54a089ae31cbe79eb35
Rokas214 <rokas.guzys@gmail.com>
2022-09-13 11:55:01 +0300

    hidden config details

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const auth = require("./src/routes/v1/auth");
 const home = require("./src/routes/v1/home");
 const gifHistory = require("./src/routes/v1/gifHistory");
